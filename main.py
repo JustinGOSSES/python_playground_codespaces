265abaeb570194b5bb990985fd3ec14afda86f49
Justin Gosses <11600445+JustinGOSSES@users.noreply.github.com>
2022-12-25 03:27:19 +0000

    Setting up examples of how to run things

---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+## This is a variable
+MESSAGE_ONE = "hello world!"
+
+### Here we print the variables MESSAGE_ONE into the console or sometimes called terminal.
+print(MESSAGE_ONE)
